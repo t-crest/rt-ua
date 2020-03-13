eed0d011281e782372381e8e2bfffa5500bd8b95
patmos <a.kirchberger@kbit.pro>
2020-03-13 12:13:42 +0100

    Initial open62541 Patmos commit, open62541 submodule in folder extern

---
+++ open62541_patmos.c
@@ -0,0 +1,316 @@
+/*
+	Demo file for open62541 Test
+
+	Author: Andreas Kirchberger
+	Copyright:
+*/
+
+#define PUB_INTERVAL             100 /* Publish interval in milliseconds */
+#define DATA_SET_WRITER_ID       62541
+
+#define MILLI_AS_NANO_SECONDS    (1000 * 1000)
+#define SECONDS_AS_NANO_SECONDS  (1000 * 1000 * 1000)
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h> //memset
+#include <machine/patmos.h>
+#include <machine/spm.h>
+#include "udp.h"
+#include "eth_mac_driver.h"
+
+#include <open62541/plugin/log_stdout.h>
+#include <open62541/plugin/pubsub_udp.h>
+#include <open62541/server.h>
+#include <open62541/server_config_default.h>
+
+#include <open62541/server_pubsub.h>
+
+UA_NodeId connectionIdent, publishedDataSetIdent, writerGroupIdent;
+
+UA_NodeId myIntegerNodeId;
+
+UA_Boolean running = true;
+
+UA_ServerCallback pubCallback = NULL;
+UA_Server *pubServer;
+void *pubData;
+
+static void
+publishInterrupt() {
+
+    //pubCallback(pubServer, pubData);
+    UA_WriterGroup_publishCallback(pubServer, pubData);
+}
+
+
+/* The following three methods are originally defined in
+ * /src/pubsub/ua_pubsub_manager.c. We provide a custom implementation here to
+ * use system interrupts instead if time-triggered callbacks in the OPC UA
+ * server control flow. */
+
+UA_StatusCode
+UA_PubSubManager_addRepeatedCallback(UA_Server *server,
+                                     UA_ServerCallback callback,
+                                     void *data, UA_Double interval_ms,
+                                     UA_UInt64 *callbackId) {
+    puts("UA_PubSubManager_addRepeatedCallback");
+
+    if(pubCallback) {
+        UA_LOG_WARNING(UA_Log_Stdout, UA_LOGCATEGORY_USERLAND,
+                       "At most one publisher can be registered for interrupt callbacks");
+        return UA_STATUSCODE_BADINTERNALERROR;
+    }
+
+    UA_LOG_INFO(UA_Log_Stdout, UA_LOGCATEGORY_USERLAND,
+                "Adding a publisher with a cycle time of %lf milliseconds", interval_ms);
+
+    /* Set global values for the publish callback */
+    int resultTimerCreate = 0;
+    pubServer = server;
+    pubCallback = callback;
+    pubData = data;
+
+    return UA_STATUSCODE_GOOD;
+}
+
+void
+UA_PubSubManager_removeRepeatedPubSubCallback(UA_Server *server, UA_UInt64 callbackId) {
+    pubCallback = NULL; /* So that a new callback can be registered */
+}
+
+/**
+ * **PublishedDataSet handling**
+ *
+ * The PublishedDataSet (PDS) and PubSubConnection are the toplevel entities and
+ * can exist alone. The PDS contains the collection of the published fields. All
+ * other PubSub elements are directly or indirectly linked with the PDS or
+ * connection. */
+static void
+addPublishedDataSet(UA_Server *server) {
+    /* The PublishedDataSetConfig contains all necessary public
+    * informations for the creation of a new PublishedDataSet */
+    UA_PublishedDataSetConfig publishedDataSetConfig;
+    memset(&publishedDataSetConfig, 0, sizeof(UA_PublishedDataSetConfig));
+    publishedDataSetConfig.publishedDataSetType = UA_PUBSUB_DATASET_PUBLISHEDITEMS;
+    publishedDataSetConfig.name = UA_STRING("Demo PDS");
+    /* Create new PublishedDataSet based on the PublishedDataSetConfig. */
+    UA_Server_addPublishedDataSet(server, &publishedDataSetConfig, &publishedDataSetIdent);
+}
+
+/**
+ * **DataSetField handling**
+ *
+ * The DataSetField (DSF) is part of the PDS and describes exactly one published
+ * field. */
+static void
+addDataSetField(UA_Server *server) {
+    /* Add a field to the previous created PublishedDataSet */
+    UA_NodeId dataSetFieldIdent;
+    UA_DataSetFieldConfig dataSetFieldConfig;
+    memset(&dataSetFieldConfig, 0, sizeof(UA_DataSetFieldConfig));
+    dataSetFieldConfig.dataSetFieldType = UA_PUBSUB_DATASETFIELD_VARIABLE;
+    dataSetFieldConfig.field.variable.fieldNameAlias = UA_STRING("Server localtime");
+    dataSetFieldConfig.field.variable.promotedField = UA_FALSE;
+    dataSetFieldConfig.field.variable.publishParameters.publishedVariable =
+    UA_NODEID_NUMERIC(0, UA_NS0ID_SERVER_SERVERSTATUS_CURRENTTIME);
+    dataSetFieldConfig.field.variable.publishParameters.attributeId = UA_ATTRIBUTEID_VALUE;
+    UA_Server_addDataSetField(server, publishedDataSetIdent,
+                              &dataSetFieldConfig, &dataSetFieldIdent);
+}
+
+/**
+ * **WriterGroup handling**
+ *
+ * The WriterGroup (WG) is part of the connection and contains the primary
+ * configuration parameters for the message creation. */
+static void
+addWriterGroup(UA_Server *server) {
+    /* Now we create a new WriterGroupConfig and add the group to the existing
+     * PubSubConnection. */
+    UA_WriterGroupConfig writerGroupConfig;
+    memset(&writerGroupConfig, 0, sizeof(UA_WriterGroupConfig));
+    writerGroupConfig.name = UA_STRING("Demo WriterGroup");
+    writerGroupConfig.publishingInterval = 100;
+    writerGroupConfig.enabled = UA_FALSE;
+    writerGroupConfig.writerGroupId = 100;
+    writerGroupConfig.encodingMimeType = UA_PUBSUB_ENCODING_UADP;
+    writerGroupConfig.rtLevel = UA_PUBSUB_RT_FIXED_SIZE;
+    /* The configuration flags for the messages are encapsulated inside the
+     * message- and transport settings extension objects. These extension
+     * objects are defined by the standard. e.g.
+     * UadpWriterGroupMessageDataType */
+    UA_Server_addWriterGroup(server, connectionIdent, &writerGroupConfig, &writerGroupIdent);
+}
+
+/**
+ * **DataSetWriter handling**
+ *
+ * A DataSetWriter (DSW) is the glue between the WG and the PDS. The DSW is
+ * linked to exactly one PDS and contains additional informations for the
+ * message generation. */
+static void
+addDataSetWriter(UA_Server *server) {
+    /* We need now a DataSetWriter within the WriterGroup. This means we must
+     * create a new DataSetWriterConfig and add call the addWriterGroup function. */
+    UA_NodeId dataSetWriterIdent;
+    UA_DataSetWriterConfig dataSetWriterConfig;
+    memset(&dataSetWriterConfig, 0, sizeof(UA_DataSetWriterConfig));
+    dataSetWriterConfig.name = UA_STRING("Demo DataSetWriter");
+    dataSetWriterConfig.dataSetWriterId = 62541;
+    dataSetWriterConfig.keyFrameCount = 10;
+    UA_Server_addDataSetWriter(server, writerGroupIdent, publishedDataSetIdent,
+                               &dataSetWriterConfig, &dataSetWriterIdent);
+}
+
+
+static UA_NodeId Counter;
+UA_UInt32 publishValue = 0xCAFEDEAD;
+
+static void
+addServerNodes(UA_Server* server) {
+    //UA_UInt32 publishValue = 0xCAFEDEAD;
+    UA_VariableAttributes publisherAttr = UA_VariableAttributes_default;
+    UA_Variant_setScalar(&publisherAttr.value, &publishValue, &UA_TYPES[UA_TYPES_INT32]);
+    publisherAttr.displayName = UA_LOCALIZEDTEXT("en-US", "My Counter");
+    publisherAttr.accessLevel = UA_ACCESSLEVELMASK_READ | UA_ACCESSLEVELMASK_WRITE;
+    myIntegerNodeId = UA_NODEID_STRING(1, "the.answer");
+    UA_Server_addVariableNode(server, myIntegerNodeId,
+                              UA_NODEID_NUMERIC(0, UA_NS0ID_OBJECTSFOLDER),
+                              UA_NODEID_NUMERIC(0, UA_NS0ID_ORGANIZES),
+                              UA_QUALIFIEDNAME(1, "My Counter"),
+                              UA_NODEID_NUMERIC(0, UA_NS0ID_BASEDATAVARIABLETYPE),
+                              publisherAttr, NULL, &Counter);
+}
+
+static void
+addPubSubConfiguration(UA_Server* server) {
+
+    UA_PubSubConnectionConfig connectionConfig;
+    memset(&connectionConfig, 0, sizeof(connectionConfig));
+    connectionConfig.name = UA_STRING("UADP Connection");
+    connectionConfig.transportProfileUri =
+        UA_STRING("http://opcfoundation.org/UA-Profile/Transport/pubsub-udp-uadp");
+    connectionConfig.enabled = true;
+    UA_NetworkAddressUrlDataType networkAddressUrl =
+        {UA_STRING_NULL, UA_STRING("opc.udp://224.0.0.22:4840/")};
+    UA_Variant_setScalar(&connectionConfig.address, &networkAddressUrl,
+                         &UA_TYPES[UA_TYPES_NETWORKADDRESSURLDATATYPE]);
+    connectionConfig.publisherId.numeric = UA_UInt32_random();
+    UA_StatusCode ret=UA_Server_addPubSubConnection(server, &connectionConfig, &connectionIdent);
+    printf("UA_Server_addPubSubConnection=%#lx\n", ret);
+
+    addPublishedDataSet(server);
+
+/*
+    UA_NodeId dataSetFieldIdent;
+    UA_DataSetFieldConfig dataSetFieldConfig;
+    memset(&dataSetFieldConfig, 0, sizeof(UA_DataSetFieldConfig));
+    dataSetFieldConfig.dataSetFieldType = UA_PUBSUB_DATASETFIELD_VARIABLE;
+    dataSetFieldConfig.field.variable.fieldNameAlias = UA_STRING("Server localtime");
+    dataSetFieldConfig.field.variable.promotedField = UA_FALSE;
+    dataSetFieldConfig.field.variable.publishParameters.publishedVariable =
+    UA_NODEID_NUMERIC(0, UA_NS0ID_SERVER_SERVERSTATUS_CURRENTTIME);
+    dataSetFieldConfig.field.variable.publishParameters.attributeId = UA_ATTRIBUTEID_VALUE;
+    UA_Server_addDataSetField(server, publishedDataSetIdent,
+                            &dataSetFieldConfig, &dataSetFieldIdent);
+*/
+
+    addServerNodes(server);
+
+    UA_NodeId dataSetFieldIdent;
+    UA_DataSetFieldConfig dataSetFieldConfig;
+    memset(&dataSetFieldConfig, 0, sizeof(UA_DataSetFieldConfig));
+    dataSetFieldConfig.dataSetFieldType = UA_PUBSUB_DATASETFIELD_VARIABLE;
+    dataSetFieldConfig.field.variable.fieldNameAlias = UA_STRING("My Config");
+    dataSetFieldConfig.field.variable.promotedField = UA_FALSE;
+    dataSetFieldConfig.field.variable.publishParameters.publishedVariable = Counter;
+    dataSetFieldConfig.field.variable.publishParameters.attributeId = UA_ATTRIBUTEID_VALUE;
+    UA_Server_addDataSetField(server, publishedDataSetIdent, &dataSetFieldConfig, &dataSetFieldIdent);
+
+    addDataSetField(server);
+    addWriterGroup(server);
+    addDataSetWriter(server);
+}
+
+
+// foo is the analysis entry point that would be inlined with -O2
+int foo(int b, int val, int val2) __attribute__((noinline));
+int foo(int b, int val, int val2) {
+
+  int i;
+  int xy;
+
+  _Pragma("loopbound min 10 max 15")
+  for (i=0; i<100; ++i) {
+    val = val + val2;
+//    printf("X");
+  }
+
+  return val;
+}
+
+// The compiler shall not compute the result
+volatile int seed2 = 3;
+
+int main()
+{
+    static unsigned char my_ip[4] = {192, 168, 2, 2};
+
+    int val = seed2;
+    int val2 = seed2+seed2;
+    int b = seed2/4;
+    int i = foo(b, val, val2);
+    printf("%i\n",i);
+
+    eth_mac_initialize();
+    arp_table_init();
+    ipv4_set_my_ip(my_ip);
+    printf("MAC: inti'd\n");
+    printf("ARP: init'd\n");
+
+    puts("\nOpen6541 Server Demo Started\n");
+
+    UA_Server *server = UA_Server_new();
+    UA_ServerConfig *config = UA_Server_getConfig(server);
+    //UA_ServerConfig_setDefault(config);
+
+    config->logger.log = UA_Log_Stdout_log;
+    config->logger.context = NULL;
+    config->logger.clear = UA_Log_Stdout_clear;
+
+    puts("UA_Server_getConfig\n");
+
+    config->pubsubTransportLayers = (UA_PubSubTransportLayer *)
+        UA_malloc(sizeof(UA_PubSubTransportLayer));
+    config->pubsubTransportLayers[0] = UA_PubSubTransportLayerUDPMP();
+    config->pubsubTransportLayersSize++;
+
+    addPubSubConfiguration(server);
+    addServerNodes(server);
+
+    UA_Int32 myInteger = 43;
+    while(1)
+    {
+        publishInterrupt();
+        sleep(1);
+
+        // Int32
+        myInteger++;
+
+        // Write a different integer value
+        UA_Variant myVar;
+        UA_Variant_init(&myVar);
+        UA_Variant_setScalar(&myVar, &myInteger, &UA_TYPES[UA_TYPES_INT32]);
+        UA_Server_writeValue(server, myIntegerNodeId, myVar);
+
+        //counterNodePublisher.namespaceIndex++;
+    }
+
+    // Run the server
+    UA_StatusCode retval = UA_Server_run(server, &running);
+    //UA_Server_delete(server);
+    //UA_ServerConfig_delete(config);
+
+    printf("\nOpen6541 Server Demo Finished");
+}
