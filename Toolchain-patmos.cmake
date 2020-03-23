29b83dab8678075c453242cf9d16fccb2250035d
patmos <a.kirchberger@kbit.pro>
2020-03-23 20:39:34 +0100

    add patmos flashing(make run), compile fixes

--- Toolchain-patmos.cmake
+++ Toolchain-patmos.cmake
@@ -1,6 +1,6 @@
 set(PATMOSHOME ~/t-crest/patmos)
 set(LIBETH ${PATMOSHOME}/c/ethlib)
+set(SERIAL /dev/ttyUSB0)
 set(CMAKE_C_COMPILER /home/patmos/t-crest/local/bin/patmos-clang)
 set(CMAKE_CXX_COMPILER /home/patmos/t-crest/local/bin/patmos-clang++)
 set(UA_ARCHITECTURE "patmos" CACHE STRING "Select architecture patmos,..")
-
