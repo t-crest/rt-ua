b8777f3b6f58e39c904d4e8867ebb33742b778df
thomasfruehwirth <thomas.fruehwirth@tuwien.ac.at>
2020-09-11 09:49:21 +0200

    added an example for jumptables with return value void

---
+++ test/wcet-code-transformation-rules/jumptable_void.c
@@ -0,0 +1,45 @@
+#include <stdio.h>
+
+#define DESIGN_PATTERN 0
+
+// void returning jumtables can't be analyzed!?
+void func0 (void) { ; }
+void func1 (void) { ; }
+
+void (*pf[])(void) = {func0, func1};
+
+#if DESIGN_PATTERN
+// jump table with design pattern applied
+void jumpTableVoid(int c)
+{
+    switch (c) {
+        case 0:
+            func0();
+            break;
+        case 1:
+            func1();
+            break;
+        default:
+            break;
+    }
+}
+#else
+// original jumptable function
+void jumpTableVoid(int c)
+{
+    pf[c]();
+}
+#endif
+
+
+void jumpTableVoid_test(int c)
+{
+    jumpTableVoid(c);
+}
+
+int main(int argc, char *argv[])
+{
+    jumpTableVoid_test(argc%2);
+
+    return 0;
+}
